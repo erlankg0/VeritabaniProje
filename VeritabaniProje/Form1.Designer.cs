87de3923a969a1a0cd5a1e5e09a12d9648a2cd0f
Erlan <era.ab.02@gmail.com>
2021-06-19 00:48:32 +0300

    Добавьте файлы проекта.

---
+++ VeritabaniProje/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace VeritabaniProje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAc = new System.Windows.Forms.Button();
+            this.btnKapat = new System.Windows.Forms.Button();
+            this.dataGridListele = new System.Windows.Forms.DataGridView();
+            this.btn_listeme = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridListele)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAc
+            // 
+            this.btnAc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAc.Location = new System.Drawing.Point(733, 51);
+            this.btnAc.Name = "btnAc";
+            this.btnAc.Size = new System.Drawing.Size(93, 64);
+            this.btnAc.TabIndex = 0;
+            this.btnAc.Text = "Veritabanını Aç";
+            this.btnAc.UseVisualStyleBackColor = true;
+            this.btnAc.Click += new System.EventHandler(this.btnAc_Click);
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKapat.Location = new System.Drawing.Point(733, 12);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(93, 33);
+            this.btnKapat.TabIndex = 1;
+            this.btnKapat.Text = "Veritabanını Kapat";
+            this.btnKapat.UseVisualStyleBackColor = true;
+            this.btnKapat.Click += new System.EventHandler(this.btnKapat_Click);
+            // 
+            // dataGridListele
+            // 
+            this.dataGridListele.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridListele.Location = new System.Drawing.Point(12, 77);
+            this.dataGridListele.Name = "dataGridListele";
+            this.dataGridListele.Size = new System.Drawing.Size(494, 254);
+            this.dataGridListele.TabIndex = 2;
+            this.dataGridListele.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btn_listeme
+            // 
+            this.btn_listeme.Location = new System.Drawing.Point(13, 12);
+            this.btn_listeme.Name = "btn_listeme";
+            this.btn_listeme.Size = new System.Drawing.Size(75, 40);
+            this.btn_listeme.TabIndex = 3;
+            this.btn_listeme.Text = "Listeleme";
+            this.btn_listeme.UseVisualStyleBackColor = true;
+            this.btn_listeme.Click += new System.EventHandler(this.btn_listeme_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 369);
+            this.Controls.Add(this.btn_listeme);
+            this.Controls.Add(this.dataGridListele);
+            this.Controls.Add(this.btnKapat);
+            this.Controls.Add(this.btnAc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridListele)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAc;
+        private System.Windows.Forms.Button btnKapat;
+        private System.Windows.Forms.DataGridView dataGridListele;
+        private System.Windows.Forms.Button btn_listeme;
+    }
+}
+
---
+++ VeritabaniProje/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;    //Access Veritabanı Kütüphanesi Tanımlama
+
+namespace VeritabaniProje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Bağlantı Cümleciği
+        OleDbConnection Baglanti = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=vt.accdb");
+
+        //Veritabanı bağlatısını kontrollü şekilde açan method
+        public void BaglantiAc()
+        {
+            try
+            {
+                Baglanti.Open(); //Veritabanını Aç
+            }
+            catch (Exception Hata)
+            {
+                MessageBox.Show(Hata.Message,"Bağlantı Açma Hatası");
+            }        
+        }
+
+        // Personel KayiListeleme Methodu
+        public void KayitListele()
+        {
+            try
+            {
+                BaglantiAc();                               // veri tabani ac
+                DataSet dataSet = new DataSet();            // dataSet olurturdum
+                string Sorgu = "Select * from Personel";     // sql sorgu olusturdum
+                OleDbDataAdapter dbDataAdapter = new OleDbDataAdapter(Sorgu, Baglanti); // data adapter olusturduk 
+                dbDataAdapter.Fill(dataSet, "Personel");  // datasete  Personel ile doldurduk,  adapter ile ama goremioyruz(yani copy var)
+                dataGridListele.DataSource = dataSet.Tables["Personel"]; // dataset deki veri datagride goster
+                Baglanti.Close();
+
+            }
+            catch (Exception Error)
+            {
+                MessageBox.Show(Error.Message, "Hata Listeleme ");
+            }
+        }
+
+        private void btnAc_Click(object sender, EventArgs e)
+        {
+            BaglantiAc();
+        }
+
+        private void btnKapat_Click(object sender, EventArgs e)
+        {
+            Baglanti.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btn_listeme_Click(object sender, EventArgs e)
+        {
+            KayitListele();
+        }
+    }
+}
